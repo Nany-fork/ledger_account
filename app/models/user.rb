69dbbfc5aadf6d508137a9da728e270c8f914296
Mel <melanievillalobos016ags@gmail.com>
2022-01-29 14:01:30 -0600

    add relation with account and user model, create account after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :account
+  after_create :create_account
+
+  def create_account
+    @user = User.last
+    @account = Account.new
+    @user.account = @account
+    @user.save
+    @account.save
+  end
 end
