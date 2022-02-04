23bb8d47d82595b0a731f6b1dcfcec06fb924d8a
Mel <melanievillalobos016ags@gmail.com>
2022-02-04 12:26:40 -0600

    Clean controller and put the login in model movement

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,34 +1,32 @@
 class MovementsController < ApplicationController
   before_action :set_movement, only: %i[ show edit update destroy ]
   after_action :authenticate_user!
-  # GET /movements or /movements.json
+ 
   def index
-    # @movements = Movement.all
     account = current_user.account
-    @movements = Movement.where(account_id: account.id)
+    @movements = account.movements
     @account_balance = current_user.account.balance
-    # @movement_type = MovementType.where(id: @movements.movement_category_id)
   end
 
-  # GET /movements/1 or /movements/1.json
+
   def show
   end
 
-  # GET /movements/new
+ 
   def new
     @movement = Movement.new
   end
 
-  # GET /movements/1/edit
+
   def edit
   end
 
-  # POST /movements or /movements.json
+ 
   def create
-    if params[:movement][:movement_type_id] == '1'
-       account = current_user.account
-       account.update(balance: account.balance + params[:movement][:ammount].to_i )
-    end
+    # if params[:movement][:movement_type_id] == '1'
+    #    account = current_user.account
+    #    account.update(balance: account.balance + params[:movement][:ammount].to_i )
+    # end
     @movement = Movement.new(movement_params.merge(account: current_user.account))
     # @movement.account = current_user.account
     respond_to do |format|
@@ -40,7 +38,6 @@ class MovementsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /movements/1 or /movements/1.json
   def update
     respond_to do |format|
       if @movement.update(movement_params)
@@ -51,7 +48,7 @@ class MovementsController < ApplicationController
     end
   end
 
-  # DELETE /movements/1 or /movements/1.json
+ 
   def destroy
     @movement.destroy
 
@@ -61,12 +58,11 @@ class MovementsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_movement
       @movement = Movement.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def movement_params
       params.require(:movement).permit(:name, :description, :date, :ammount, :category, :movement_type_id )
     end
