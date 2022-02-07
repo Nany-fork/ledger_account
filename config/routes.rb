7763b606a45d14861281b3de30c801486c673396
Mel <melanievillalobos016ags@gmail.com>
2022-02-07 11:52:15 -0600

    MovementTypes remove controller and views , create seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :movements
   resources :movement_categories
-  resources :movement_types
   devise_for :users
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# MovementTYpes Defined
 MovementType.create([{id:1,name: "Income"},{id:2,name: "Egress"},{id:3,name: "Credit"},{id:4,name: "Payment"},{id:5,name: "Investment"}])
-MovementCategory.create([{name: "Beauty",movement_type_id:1},{name: "Beauty",movement_type_id:2},{name: "Health",movement_type_id:2},{name: "Health",movement_type_id:1},
-{name: "Electronics",movement_type_id:1},{name: "Electronics",movement_type_id:2},{name: "Food",movement_type_id:2},{name: "Food",movement_type_id:1},{name: "Home",movement_type_id:2},
-{name: "Maintenance",movement_type_id: 2},{name: "Maintenance",movement_type_id: 1},{name: "Others",movement_type_id:1},{name: "Others",movement_type_id:2},{name: "Credit cards", movement_type_id:3},{name: "Credit cards", movement_type_id:4},
-{name: "Department cards",movement_type_id: 4},{name: "Department cards",movement_type_id: 3},{name: "Personal loans",movement_type_id:3},{name: "Personal loans",movement_type_id:4},{name: "Mortgage credit",movement_type_id:4},{name: "Mortgage credit",movement_type_id:3},
-{name: "Mortgage credit",movement_type_id: 3},{name: "Mortgage credit",movement_type_id: 4},{name: "Real estate",movement_type_id:5},{name: "Cryptocurrency",movement_type_id:5},{name: "Cetes",movement_type_id:5},{name: "Shares of stock",movement_type_id:5}])
