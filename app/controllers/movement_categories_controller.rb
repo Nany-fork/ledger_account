727405713b25edf048c0b4c276515f47a2ff6b93
Nany-fork <95006480+Nany-fork@users.noreply.github.com>
2022-02-08 19:21:01 -0600

    Asyn form movements (#12)
    ## Form asyn for movemetns
    - you can select movement type and depending of the movement categories are displayed
    - Add new categories

--- app/controllers/movement_categories_controller.rb
+++ app/controllers/movement_categories_controller.rb
@@ -1,38 +1,30 @@
 class MovementCategoriesController < ApplicationController
   before_action :set_movement_category, only: %i[ show edit update destroy ]
   after_action :authenticate_user!
-  # GET /movement_categories or /movement_categories.json
-  def index
-    @movement_categories = MovementCategory.all
-  end
-
-  # GET /movement_categories/1 or /movement_categories/1.json
   def show
+    @movement_categories = MovementCategory.select(:id,:name).where(movement_type_id:params[:id])
+    render json: @movement_categories
   end
 
-  # GET /movement_categories/new
   def new
     @movement_category = MovementCategory.new
   end
 
-  # GET /movement_categories/1/edit
   def edit
   end
 
-  # POST /movement_categories or /movement_categories.json
   def create
     @movement_category = MovementCategory.new(movement_category_params)
 
     respond_to do |format|
       if @movement_category.save
-        format.html { redirect_to movement_category_url(@movement_category), notice: "Movement category was successfully created." }
+        format.html { redirect_to new_movement_path , notice: "Movement category was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /movement_categories/1 or /movement_categories/1.json
   def update
     respond_to do |format|
       if @movement_category.update(movement_category_params)
@@ -43,7 +35,6 @@ class MovementCategoriesController < ApplicationController
     end
   end
 
-  # DELETE /movement_categories/1 or /movement_categories/1.json
   def destroy
     @movement_category.destroy
 
@@ -53,12 +44,11 @@ class MovementCategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_movement_category
       @movement_category = MovementCategory.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+
     def movement_category_params
       params.require(:movement_category).permit(:name, :movement_type_id)
     end
---
+++ app/javascript/movement_category.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const movementSelect = document.querySelector("#movement_movement_type_id");
+    const movementFilt = document.querySelector("#movement_category");
+    movementSelect.addEventListener("change", function (e) {
+      movementFilt.innerHTML=''
+      movementFilt.value=''
+      const selectedMovement = e.target.value;
+      console.log(selectedMovement)
+      fetch(`/movement_categories/${selectedMovement}`)
+        .then((response) => response.json())
+        .then((data) => {
+          for (const categoria of data) {
+            var opt = document.createElement("option");
+            opt.value = categoria.name;
+            opt.innerHTML = categoria.name;
+            movementFilt.appendChild(opt);
+          }
+        });
+    });
+  });
+  
