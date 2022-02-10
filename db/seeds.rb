1a552be2dd526a35541957f635a56aa20257c85f
Nany-fork <95006480+Nany-fork@users.noreply.github.com>
2022-02-10 10:50:10 -0600

    fixing the database seeds (#15)
    * fixing the database seeds
    
    * fixing the database seeds 2.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
-# MovementTYpes Defined
-MovementType.create([{ id: 1, name: "Income" }, { id: 2, name: "Egress" }, { id: 3, name: "Credit" }, { id: 4, name: "Payment" }, { id: 5, name: "Investment" }])
+income_movement = MovementType.find_or_create_by(name: "Income")
+egress_movement = MovementType.find_or_create_by(name: "Egress")
+credit_movement = MovementType.find_or_create_by(name: "Credit")
+payment_movement = MovementType.find_or_create_by(name: "Payment")
+investment_movement = MovementType.find_or_create_by(name:"Investment")
+MovementCategory.create([{ name: "Beauty", movement_type_id: egress_movement.id }, { name: "Health",movement_type_id: egress_movement.id }, { name: "Electronics",movement_type_id: egress_movement.id }, { name: "Food",movement_type_id: egress_movement.id }, { name: "Home",movement_type_id: egress_movement.id }, { name: "Maintenance",movement_type_id: egress_movement.id }, { name: "Others", movement_type_id: egress_movement.id }])
+MovementCategory.create([{ name: "Beauty", movement_type_id: income_movement.id }, { name: "Health",movement_type_id: income_movement.id }, { name: "Electronics",movement_type_id: income_movement.id }, { name: "Food",movement_type_id: income_movement.id }, { name: "Home",movement_type_id: income_movement.id }, { name: "Maintenance",movement_type_id: income_movement.id }, { name: "Others", movement_type_id: income_movement.id}]) 
+MovementCategory.create([{ name: "Credit", movement_type_id: payment_movement.id }, { name: "Department", movement_type_id: payment_movement.id }, { name: "Personal", movement_type_id: payment_movement.id }, { name: "Mortgage", movement_type_id: payment_movement.id }])
+MovementCategory.create([{ name: "Credit", movement_type_id: credit_movement.id }, { name: "Department", movement_type_id: credit_movement.id }, { name: "Personal", movement_type_id: credit_movement.id }, { name: "Mortgage", movement_type_id: credit_movement.id }])
+MovementCategory.create([{ name: "RealEstate", movement_type_id: investment_movement.id }, { name: "Cryptocurrency", movement_type_id: investment_movement.id }, { name: "Cetes", movement_type_id: investment_movement.id }, { name: "Stock", movement_type_id:investment_movement.id}])
 
-MovementCategory.create([{ name: "Beauty", movement_type_id: 1 }, { name: "Beauty", movement_type_id: 2 }, { name: "Health", movement_type_id: 2 }, { name: "Health", movement_type_id: 1 },
-                         { name: "Electronics", movement_type_id: 1 }, { name: "Electronics", movement_type_id: 2 }, { name: "Food", movement_type_id: 2 }, { name: "Food", movement_type_id: 1 }, { name: "Home", movement_type_id: 2 },
-                         { name: "Maintenance", movement_type_id: 2 }, { name: "Maintenance", movement_type_id: 1 }, { name: "Others", movement_type_id: 1 }, { name: "Others", movement_type_id: 2 }, { name: "Credit cards", movement_type_id: 3 }, { name: "Credit cards", movement_type_id: 4 },
-                         { name: "Department cards", movement_type_id: 4 }, { name: "Department cards", movement_type_id: 3 }, { name: "Personal loans", movement_type_id: 3 }, { name: "Personal loans", movement_type_id: 4 }, { name: "Mortgage credit", movement_type_id: 4 }, { name: "Mortgage credit", movement_type_id: 3 },
-                         { name: "Mortgage credit", movement_type_id: 3 }, { name: "Mortgage credit", movement_type_id: 4 }, { name: "Real estate", movement_type_id: 5 }, { name: "Cryptocurrency", movement_type_id: 5 }, { name: "Cetes", movement_type_id: 5 }, { name: "Shares of stock", movement_type_id: 5 }])
+
+
